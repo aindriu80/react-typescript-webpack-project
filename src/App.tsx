be7f498349ae9d6c5f5564d2ed680292c45c2f62
aindriu80 <aindriu80@gmail.com>
2021-03-27 12:05:49 +0000

    Configure webpack for mulitple environments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import IMAGE2 from "./skyscraper.webp";
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template </h1>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
       <h2>Rendering a .jpg with webpack</h2>
       <img src={IMAGE} alt="" width="300" height="300" />
       <h2>Rendering a .webp with webpack</h2>
