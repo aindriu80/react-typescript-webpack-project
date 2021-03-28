a09741d167502bc773aab0a09e000b16c4662624
aindriu80 <aindriu80@gmail.com>
2021-03-28 11:09:44 +0100

    React Refresh Webpack Plugin - hot reloading of a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./styles.css";
 import IMAGE from "./react-image.jpg";
 import IMAGE2 from "./skyscraper.webp";
+import React from "react";
+import { ClickCounter } from "./ClickCounter";
 export const App = () => {
   return (
     <>
@@ -8,10 +10,12 @@ export const App = () => {
         React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
         {process.env.name}
       </h1>
+      <h2>React Refresh Webpack Plug</h2>
       <h2>Rendering a .jpg with webpack</h2>
       <img src={IMAGE} alt="" width="300" height="300" />
       <h2>Rendering a .webp with webpack</h2>
       <img src={IMAGE2} alt="" width="300" height="300" />
+      <ClickCounter />
     </>
   );
 };
